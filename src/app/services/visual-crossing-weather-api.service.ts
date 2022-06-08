0874cfc275fe1626384643641204db70fa47d55c
Stanislav Semenchuk <ssemenchuk@processtech.ru>
2022-06-08 10:06:23 +0300

    refactor: extract base url to config

--- src/app/services/visual-crossing-weather-api.service.ts
+++ src/app/services/visual-crossing-weather-api.service.ts
@@ -15,11 +15,8 @@ export class VisualCrossingWeatherApiService implements WeatherApiService {
 
   public getWeather(city: string, day: Date): Observable<Weather[]> {
     const isoDate = day.toISOString().slice(0, VisualCrossingWeatherApiService.ISO_TIME_PART_LENGTH);
-    // TODO configure base url - https://juristr.com/blog/2016/11/configure-proxy-api-angular-cli/
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/${isoDate}`;
-    const params = new HttpParams()
-      .append('key', VisualCrossingWeatherApiService.API_KEY);
-    return this.httpClient.get(url, { params })
+    const params = { key: VisualCrossingWeatherApiService.API_KEY };
+    return this.httpClient.get(`/VisualCrossingWebServices/rest/services/timeline/${city}/${isoDate}`, { params })
       .pipe(map(this.jsonToModel));
   }
 
