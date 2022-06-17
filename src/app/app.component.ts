61836ac2d46fd071658b0243a72a9dbcf51ecac9
Stanislav Semenchuk <undefy23@gmail.com>
2022-06-17 11:00:12 +0300

    fix: binding problem - "changeDetection" set to default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { mergeMap } from 'rxjs';
 import { Weather } from './model/weather.model';
 import { GeoLocationService } from './services/geo-location.service';
@@ -8,23 +8,18 @@ import { WeatherApiService } from './services/weather-api.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
   private static readonly ISO_DATE_LENGTH = 10;
 
   city: string;
   forecast: Map<string, Weather[]>;
+  // TODO extract to method?
   todayWeather: Weather[];
-  contentLoaded: boolean;
+  backgroundImageClass: string;
 
-  private today: Date;
-
-  constructor(
-    private apiService: WeatherApiService,
-    private locationService: GeoLocationService,
-    private renderer: Renderer2
-  ) {}
+  constructor (private apiService: WeatherApiService, private locationService: GeoLocationService) {
+  }
 
   // TODO handle errors for calls to services
   ngOnInit(): void {
@@ -34,14 +29,9 @@ export class AppComponent implements OnInit {
   }
 
   private updateView = (forecast: Map<string, Weather[]>): void => {
-    this.contentLoaded = false;
-    this.today = new Date;
+    const today = new Date;
     this.forecast = forecast;
-    this.todayWeather = this.forecast.get(this.today.toISOString().slice(0, AppComponent.ISO_DATE_LENGTH)) || [];
-    const backgroundImageClass = `bg-${this.todayWeather[this.today.getHours()].decoration}`;
-    this.renderer.addClass(document.getElementById('background-back'), backgroundImageClass);
-    this.renderer.addClass(document.getElementById('background-front'), backgroundImageClass);
-    this.contentLoaded = true;
-    console.dir(this.forecast);
+    this.todayWeather = this.forecast.get(today.toISOString().slice(0, AppComponent.ISO_DATE_LENGTH)) || [];
+    this.backgroundImageClass = `bg-${this.todayWeather[today.getHours()].decoration}`;
   };
 }
