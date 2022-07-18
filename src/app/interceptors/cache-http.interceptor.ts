e38bb27f1dfe646243d02bf1cbca550a1f2e9c1d
Stanislav Semenchuk <undefy23@gmail.com>
2022-07-18 13:27:51 +0300

    fix: cache interceptor, use body and params for hash

--- src/app/interceptors/cache-http.interceptor.ts
+++ src/app/interceptors/cache-http.interceptor.ts
@@ -16,13 +16,22 @@ export class CacheHttpInterceptor implements HttpInterceptor {
   private static readonly DATE_AND_HOURS_LENGTH = 13;
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const hash = request.url + new Date().toISOString().slice(0, CacheHttpInterceptor.DATE_AND_HOURS_LENGTH);
+    const hash = this.hashFromRequest(request);
     const cached = localStorage.getItem(hash) || '';
     if (cached) {
       return of(new HttpResponse(JSON.parse(cached)));
     }
     return next.handle(request)
       .pipe(tap(event => event instanceof HttpResponse && this.setItem(hash, JSON.stringify(event))));
+  }
+
+  private hashFromRequest(request: HttpRequest<unknown>): string {
+    const date = new Date().toISOString().slice(0, CacheHttpInterceptor.DATE_AND_HOURS_LENGTH);
+    console.dir(request);
+    if (request.method === 'GET') {
+      return request.urlWithParams + date;
+    }
+    return request.url + request.body + date;
   }
 
   private setItem(key: string, value: string): void {
