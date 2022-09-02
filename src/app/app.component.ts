65b043b9e0a9164756f362050f95580458b604f3
Stanislav Semenchuk <undefy23@gmail.com>
2022-09-02 14:54:29 +0300

    refactor: move geolocation logic to the root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import {TopBarComponent} from './components/top-bar/top-bar.component';
+import { GeolocationService } from './services/geolocation.service';
 import { WeatherApiService } from './services/weather-api.service';
+import { ErrorState } from './state/error-state';
 import { LoadingStateManager } from './state/loading-state-manager';
 import { ObservableCity } from './state/observable-city';
 
@@ -10,20 +15,50 @@ import { ObservableCity } from './state/observable-city';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  @ViewChild('top_bar') topBar: TopBarComponent;
+
   weatherBeingLoaded: boolean;
 
   constructor(
     private observableCity: ObservableCity,
     private title: Title,
+    private activatedRout: ActivatedRoute,
     private weatherService: WeatherApiService,
     private loadingStateManager: LoadingStateManager,
+    private geolocationService: GeolocationService,
+    private errorState: ErrorState,
   ) {
     this.weatherBeingLoaded = true;
   }
 
   ngOnInit(): void {
-    this.observableCity.onChange(c => this.title.setTitle(`Weather – ${c}`));
+    this.tryToDetectCity();
+    this.activatedRout.queryParams.subscribe(params => {
+      const city = params['city'];
+      if (city) {
+        this.observableCity.update(city);
+        this.title.setTitle(`Weather – ${city}`);
+      }
+    });
     this.loadingStateManager.getStateByKey(this.weatherService.getOrigin())
       .onChange(bl => this.weatherBeingLoaded = bl);
+  }
+
+  private tryToDetectCity(): void {
+    const queryParams = new HttpParams({ fromString: window.location.search });
+    if (queryParams.get('city')) { // if the url already contains a city param
+      return;
+    }
+    this.geolocationService.detectCity().subscribe({
+      next: c => this.topBar.navigateToCity(c),
+      error: e => {
+        this.errorState.riseError({
+          problem: 'An error occured trying to detect your city:',
+          message: e.message,
+          advice: 'Please, press "another city" and type the city name you need'
+        });
+        this.topBar.navigateToCity(GeolocationService.DEFAULT_CITY);
+      }
+    });
   }
 }
--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -3,12 +3,10 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ObservableCity } from 'src/app/state/observable-city';
 import { datetimeByTimezoneOffset } from 'src/app/util/datetime-util';
 import { GeolocationService } from 'src/app/services/geolocation.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { HttpParams } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { ObservableCurrentConditions } from 'src/app/state/observable-current-conditions';
 import { CurrentConditions } from 'src/app/model/current-conditions.model';
 import { LoadingStateManager } from 'src/app/state/loading-state-manager';
-import { ErrorState } from 'src/app/state/error-state';
 import { CityAutocompleteComponent } from '../city-autocomplete/city-autocomplete.component';
 
 const DELAY_BEFORE_CLOSE_INPUT = 300;
@@ -31,23 +29,15 @@ export class TopBarComponent implements OnInit {
   constructor(
     private geolocationService: GeolocationService,
     private router: Router,
-    private activatedRout: ActivatedRoute,
-    private errorState: ErrorState,
     private observableCity: ObservableCity,
     private observableCurrentConditions: ObservableCurrentConditions,
     private loadingStateManager: LoadingStateManager,
   ) {}
 
   ngOnInit(): void {
-    this.tryToDetectCity();
-    // TODO try to move to the app component
-    this.activatedRout.queryParams.subscribe(params => {
-      const city = params['city'];
-      if (city) {
-        this.showInput = false;
-        this.city = city;
-        this.observableCity.update(city);
-      }
+    this.observableCity.onChange(c => {
+      this.showInput = false;
+      this.city = c;
     });
     this.observableCurrentConditions.onChange(cc => {
       this.currentConditions = cc;
@@ -77,23 +67,5 @@ export class TopBarComponent implements OnInit {
       this.showInput = false;
       this.cityAutocomplete.clear();
     }, DELAY_BEFORE_CLOSE_INPUT);
-  }
-
-  private tryToDetectCity(): void {
-    const queryParams = new HttpParams({ fromString: window.location.search });
-    if (queryParams.get('city')) { // if the url already contains a city param
-      return;
-    }
-    this.geolocationService.detectCity().subscribe({
-      next: c => this.navigateToCity(c),
-      error: e => {
-        this.errorState.riseError({
-          problem: 'An error occured trying to detect your city:',
-          message: e.message,
-          advice: 'Please, press "another city" and type the city name you need'
-        });
-        this.navigateToCity(GeolocationService.DEFAULT_CITY);
-      }
-    });
   }
 }
