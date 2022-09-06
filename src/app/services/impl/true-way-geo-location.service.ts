12bb881f5ad54d7ec2891bfcd45e88c6fd30cbdc
Stanislav Semenchuk <undefy23@gmail.com>
2022-09-06 19:26:46 +0300

    refactor: location service - use Observable instead of Promise

--- src/app/services/impl/true-way-geo-location.service.ts
+++ src/app/services/impl/true-way-geo-location.service.ts
@@ -1,9 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, from, firstValueFrom } from 'rxjs';
+import { Observable, map, mergeMap } from 'rxjs';
 import { GeolocationService } from '../geolocation.service';
-
 
 /*
  * docs: https://rapidapi.com/trueway/api/trueway-geocoding/
@@ -18,20 +17,19 @@ export class TrueWayGeolocationService implements GeolocationService {
       'X-RapidAPI-Key': '61bb649c42msh7458574855e1682p16c9d8jsnbb57896d2662',
       'X-RapidAPI-Host': 'trueway-geocoding.p.rapidapi.com'
     };
-    const callback = (resolve: any, reject: any): void => {
-      navigator.geolocation.getCurrentPosition(p => resolve(p.coords), e => reject(e));
-    };
-    return from(
-      new Promise<GeolocationCoordinates>(callback)
-        .then(coords => ({ location: `${coords.latitude},${coords.longitude}`, language: 'en' }))
-        .then(params => firstValueFrom(this.httpClient.get(url, { headers, params })))
-        .then((data: any) => data.results[1].locality)
-        .catch(e => { throw e; })
-    );
+    return new Observable<GeolocationCoordinates>(observer => {
+      navigator.geolocation.getCurrentPosition(
+        p => observer.next(p.coords),
+        e => observer.error(e),
+        { timeout: 7_000 }
+      );
+    })
+      .pipe(map(coords => ({ location: `${coords.latitude},${coords.longitude}`, language: 'en' })))
+      .pipe(mergeMap(params => this.httpClient.get(url, { headers, params } )))
+      .pipe(map((data: any) => data.results[1].locality));
   }
 
   getOrigin(): string {
     return 'https://trueway-geocoding.p.rapidapi.com';
   }
 }
-
